9f9e83e841f5b70a15a6a1a1ef264a1859c4e115
Owen Chen <owen.c.learning@gmail.com>
2026-08-20 13:01:25 -0700

    minor tuning

--- examples/simulation_demo.cpp
+++ examples/simulation_demo.cpp
@@ -18,21 +18,19 @@ private:
     }
     objects = Container<Object *>();
 
-    objects.addValue(new Circle(2.0f, 1.0f, 100.0f, 0.0f, 500.0f, 25.0f, 100.0f, 100.0f));
-    objects.addValue(new Circle(2.0f, 1.0f, -100.0f, 0.0f, 500.0f, 25.0f, 325.0f, 125.0f));
+    objects.addValue(new Circle(2.0f, 1.0f, 100.0f, 0.0f, 500.0f, 25.0f, 700.0f, 300.0f));
+    objects.addValue(new Circle(2.0f, 1.0f, -100.0f, 0.0f, 500.0f, 25.0f, 350.0f, 260.0f));
 
-    objects.addValue(new Rect(2.5f, 1.0f, 100.0f, 0.0f, 500.0f, 150.0f, 190.0f, 250.0f, 240.0f));
-    objects.addValue(new Rect(2.5f, 1.0f, -100.0f, 0.0f, 500.0f, 400.0f, 220.0f, 500.0f, 270.0f));
+    objects.addValue(new Rect(2.5f, 1.0f, 100.0f, 0.0f, 500.0f, 430.0f, 20.0f, 530.0f, 70.0f));
+    objects.addValue(new Rect(2.5f, 1.0f, -100.0f, 0.0f, 500.0f, 60.0f, 470.0f, 160.0f, 520.0f));
 
-    objects.addValue(new Rect(5.0f, 1.0f, 100.0f, 0.0f, 500.0f, 250.0f, 360.0f, 350.0f, 460.0f));
-    objects.addValue(new Circle(5.0f, 1.0f, -100.0f, 0.0f, 500.0f, 50.0f, 500.0f, 335.0f));
+    objects.addValue(new Rect(5.0f, 1.0f, 100.0f, 0.0f, 500.0f, 230.0f, 10.0f, 330.0f, 110.0f));
+    objects.addValue(new Circle(5.0f, 1.0f, -100.0f, 0.0f, 500.0f, 50.0f, 510.0f, 280.0f));
 
-    objects.addValue(new Rect(0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 800.0f,
-                              10.0f));                                                      // top boundary
-    objects.addValue(new Rect(0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 590.0f, 800.0f, 600.0f)); // bottom boundary
-    objects.addValue(new Rect(0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 10.0f,
-                              600.0f));                                                     // left boundary
-    objects.addValue(new Rect(0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 790.0f, 0.0f, 800.0f, 600.0f)); // right boundary
+    objects.addValue(new Rect(0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, -10.0f, 800.0f, 0.0f));   // top boundary
+    objects.addValue(new Rect(0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 600.0f, 800.0f, 610.0f)); // bottom boundary
+    objects.addValue(new Rect(0.0f, 1.0f, 0.0f, 0.0f, 0.0f, -10.0f, 0.0f, 0.0f, 600.0f));   // left boundary
+    objects.addValue(new Rect(0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 800.0f, 0.0f, 810.0f, 600.0f)); // right boundary
   }
 };
 
